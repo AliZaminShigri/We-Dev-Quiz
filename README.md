ed19926d5e9fe1bafb514f136a88094c8285beca
ihnaqi <ihnaqi@gmail.com>
2022-11-05 12:26:21 +0500

    Task is completed
    "Assignment completed"
    
    Co-Authored-By: Zamin Ali Baltee <78224767+AliZaminShigri@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,7 +1,46 @@
 # Required Results
 
 ### Task 1
- - [click here](https://aabf.docs.apiary.io/#reference/0/books/books)
 
- ### Task 2
- - [click here](https://aabf.docs.apiary.io/#reference/0/books-search/books-search)
+- [click here](https://aabf.docs.apiary.io/#reference/0/books/books)
+
+#### Answer 1
+
+**Code**
+
+```javascript:
+app.get("/books", async (req, res) => {
+  await axios
+    .post("https://private-anon-2689b13390-aabf.apiary-mock.com/books")
+    .then((response) => {
+      console.log(response.data);
+      res.status(200).send(response.data);
+    });
+});
+```
+
+**Screenshot**
+
+[task 1 screenshot](./assets/task1.png)
+
+### Task 2
+
+- [click here](https://aabf.docs.apiary.io/#reference/0/books-search/books-search)
+
+#### Answer 2
+
+```javascript:
+app.get("/bookSearch", async (req, res) => {
+  await axios
+    .post(`https://private-anon-2689b13390-aabf.apiary-mock.com/books-search`)
+    .then((response) => {
+      console.log(response.data);
+      res.send(response.data);
+    });
+});
+
+```
+
+**Screenshot**
+
+[task 2 screenshot](./assets/task2.png)
--- app.js
+++ app.js
@@ -1,30 +1,29 @@
-const express = require('express');
-const cors = require('cors');
-const { response } = require('express');
-const axios = require('axios')
+const express = require("express");
+const cors = require("cors");
+const { response } = require("express");
+const axios = require("axios");
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(cors());
 
-const url = "http://aabf.docs.apiary.io/#reference/0/books/books/"
-app.get("/books", async (req, res) =>  {
-    await axios.get(url)
-    .then(response =>{
-        console.log(response)
-        res.status(200).send(response.body);
+app.get("/books", async (req, res) => {
+  await axios
+    .post("https://private-anon-2689b13390-aabf.apiary-mock.com/books")
+    .then((response) => {
+      console.log(response.data);
+      res.status(200).send(response.data);
     });
 });
 
 app.get("/bookSearch", async (req, res) => {
-    await axios.get(`https://aabf.docs.apiary.io/#reference/0/books-search/books-search`)
-    .then(response => {
-        console.log(response);
-        res.send(response.body);
-    })
-})
-
-
+  await axios
+    .post(`https://private-anon-2689b13390-aabf.apiary-mock.com/books-search`)
+    .then((response) => {
+      console.log(response.data);
+      res.send(response.data);
+    });
+});
 
 app.listen(9000, () => {
   console.log("Session Started at port 9000");
